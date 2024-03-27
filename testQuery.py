5d9685933c3065778750fdaa5980238c0416b9fe
winnawa <namkhoapro2804@gmail.com>
2024-03-27 17:08:32 +0700

    populate data

--- testQuery.py
+++ testQuery.py
@@ -2,10 +2,6 @@ from dataConnection import conn
 
 curr = conn.cursor()
 
-curr.execute("""DELETE FROM experiences WHERE userId = 5 """)
-
-curr.execute("""INSERT INTO experiences (userId,experienceDescription,timeline) VALUES 
-    (5,"In their role as an AI Analyst at VinAI, User conducted thorough market research and data analysis that informed the companyss AI strategy, publishing a model that can detect the malfunctioning body part of the human, thus treating the health problem before it becomes serious  . Their insights led to the successful launch of three new AI products.","2020 To 2023.")
-""")
+curr.execute("""UPDATE posts SET contactEmail = "email@gmail.com" WHERE Id < 10 """)
 
 conn.commit()
